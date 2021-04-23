f9b0fdedff5af6fb043c6812b9e7253ee45ef602
Michał Bar <logout400@gmail.com>
2021-04-23 15:05:52 +0200

    chore: add comment to responsive page

--- src/pages/responsive.tsx
+++ src/pages/responsive.tsx
@@ -5,6 +5,8 @@ import {ResponsiveBox} from "../components/ResponsiveBox";
 import {useDimensions} from "@react-native-community/hooks";
 
 export default function Responsive() {
+  // Mock implementation of breakpoint detection
+  // for native platform. Web uses media queries
   const width = useDimensions().screen.width
   const breakpoint: Breakpoint = width < 576 ? 'sm' : 'md'
 
